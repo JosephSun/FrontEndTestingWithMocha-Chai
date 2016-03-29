166a83fdd0b960a78f63036fb52923186a82a307
BKhurshid <josephsun4@gmail.com>
2016-03-28 17:51:43 -0700

    wraped my expect statement in it block

--- test/test.js
+++ test/test.js
@@ -2,5 +2,7 @@ var expect = chai.expect;
 var should = chai.should();
 
 describe('Compare Numbers', function() {
-  expect(1).to.equal(1);
+  it('1 should equal 1', function() {
+    expect(1).to.equal(1);
+  }); 
 });
