51e11ea4f1e27c611ede48e51fe0ef622df30072
BKhurshid <josephsun4@gmail.com>
2016-03-28 17:57:20 -0700

    created a it block for true return

--- test/test.js
+++ test/test.js
@@ -13,5 +13,8 @@ describe('Compare Numbers', function() {
 });
 
 describe('Is Even Tests', function() {
-  
+
+  it('Should always return a boolean', function() {
+    expect(isEven(2)).to.be.a('boolean');
+  });
 });
