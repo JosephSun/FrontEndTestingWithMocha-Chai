c99f42d7fd56f79a0948d5e6d3f229925a39583f
BKhurshid <josephsun4@gmail.com>
2016-03-28 17:52:32 -0700

    wrote a greater than test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,12 @@ var expect = chai.expect;
 var should = chai.should();
 
 describe('Compare Numbers', function() {
+
   it('1 should equal 1', function() {
     expect(1).to.equal(1);
-  }); 
+  });
+
+  it('2 should be greater than 1', function() {
+    expect(2).to.be.greaterThan(1);
+  })
 });
